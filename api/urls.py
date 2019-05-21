99cc0b3fe8563f9554dfbd2ac7215ffbc7104216
jwilson1293 <jthatcher1293@gmail.com>
2019-05-21 15:24:46 -0500

    add basic author models

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from api import views
 
 
 urlpatterns = [
+    path('authors', views.AuthorListView.as_view()),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view()),
     path('books', views.BookListView.as_view()),
-    path('books/<int:pk>', views.BookDetailView.as_view())
+    path('books/<int:pk>', views.BookDetailView.as_view()),
 ]
