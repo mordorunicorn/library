e71004bf60e365404a8c9c6f1e17f84960cc7568
jwilson1293 <jthatcher1293@gmail.com>
2019-05-21 13:06:57 -0500

    refactor: book mixin

--- api/views.py
+++ api/views.py
@@ -3,13 +3,15 @@ from rest_framework import generics
 from api import models, serializers
 
 
-class BookListView(generics.ListCreateAPIView):
+class BookMixin():
     resource_name = 'books'
     serializer_class = serializers.BookSerializer
     queryset = models.Book.objects.all()
 
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
-    resource_name = 'books'
-    serializer_class = serializers.BookSerializer
-    queryset = models.Book.objects.all()
+class BookListView(BookMixin, generics.ListCreateAPIView):
+    pass
+
+
+class BookDetailView(BookMixin, generics.RetrieveUpdateDestroyAPIView):
+    pass
