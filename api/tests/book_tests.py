5e73705e58ab0fee48b85acc969aa58b967019a6
jwilson1293 <jthatcher1293@gmail.com>
2019-05-20 22:13:11 -0500

    add additional information to books

--- api/tests/book_tests.py
+++ api/tests/book_tests.py
@@ -4,31 +4,16 @@ from django.test import TestCase
 from api.models import Book
 
 
-class BookTests(TestCase):
+class BookCreateTests(TestCase):
 
     def test_can_create_a_book(self):
-        data = {
-            'data': {
-                'type': 'books',
-                'attributes': {
-                    'title': 'The Silmarillion',
-                    'author': 'J.R.R. Tolkien'
-                }
-            }
-        }
-        response = self.client.post('/api/books', data, 'application/vnd.api+json')
+        response = self.client.post('/api/books', self.book_data, 'application/vnd.api+json')
         self.assertEqual(201, response.status_code)
         self.assertEqual(1, Book.objects.count())
 
     def test_cannot_create_book_without_title(self):
-        data = {
-            'data': {
-                'type': 'books',
-                'attributes': {
-                    'author': 'Neil Gaiman'
-                }
-            }
-        }
+        data = self.book_data
+        del data['data']['attributes']['title']
         response = self.client.post('/api/books', data, 'application/vnd.api+json')
         self.assertEqual(400, response.status_code)
         content = json.loads(response.content)
@@ -36,33 +21,66 @@ class BookTests(TestCase):
         self.assertEqual('/data/attributes/title', content['errors'][0]['source']['pointer'])
 
     def test_cannot_create_book_without_author(self):
-        data = {
-            'data': {
-                'type': 'books',
-                'attributes': {
-                    'title': 'Beowulf'
-                }
-            }
-        }
+        data = self.book_data
+        del data['data']['attributes']['author']
         response = self.client.post('/api/books', data, 'application/vnd.api+json')
         self.assertEqual(400, response.status_code)
         content = json.loads(response.content)
         self.assertEqual('This field is required.', content['errors'][0]['detail'])
         self.assertEqual('/data/attributes/author', content['errors'][0]['source']['pointer'])
 
-    def test_can_edit_a_book(self):
-        book = Book.objects.create(title='Dracula', author='Some Guy')
-        data = {
+    @property
+    def book_data(self):
+        return {
             'data': {
-                'id': str(book.pk),
                 'type': 'books',
                 'attributes': {
-                    'title': 'Dracula',
-                    'author': 'Bram Stoker'
+                    'title': 'The Fellowship of the Ring',
+                    'author': 'J.R.R. Tolkien',
+                    'series': 'The Lord of the Rings',
+                    'series_num': 1,
+                    'year': 1954,
+                    'genre': 'fantasy',
+                    'age_group': 'adult',
+                    'cover_url': 'https://images.gr-assets.com/books/1419127843l/18510.jpg'
                 }
             }
         }
-        response = self.client.patch(f'/api/books/{book.pk}', data, 'application/vnd.api+json')
-        self.assertEqual(200, response.status_code)
-        self.assertEqual(data, json.loads(response.content))
 
+
+class BookGetEditTests(TestCase):
+
+    def setUp(self):
+        self.book = Book.objects.create(title='Dracula', author='Bram Stoker', year=1897, genre='horror')
+
+    def test_can_get_a_book(self):
+        response = self.client.get(f'/api/books/{self.book.pk}')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.dracula_data, json.loads(response.content))
+
+    def test_can_edit_a_book(self):
+        data = self.dracula_data
+        data['data']['attributes']['author'] = 'Some Guy'
+        response = self.client.patch(f'/api/books/{self.book.pk}', data, 'application/vnd.api+json')
+        self.assertEqual(200, response.status_code)
+        self.assertEqual('Some Guy', json.loads(response.content)['data']['attributes']['author'])
+
+    @property
+    def dracula_data(self):
+        return {
+            'data': {
+                'id': str(self.book.pk),
+                'type': 'books',
+                'attributes': {
+                    'title': 'Dracula',
+                    'author': 'Bram Stoker',
+                    'year': 1897,
+                    'genre': 'horror',
+                    'read': False,
+                    'series': None,
+                    'series_num': None,
+                    'age_group': 'adult',
+                    'cover_url': None
+                }
+            }
+        }
