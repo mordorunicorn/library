10b2f00061cd223da5964ea7673e82c733575633
Caleb Salt <voidnologo@users.noreply.github.com>
2019-10-01 10:48:06 -0400

    f-string the string

--- api/models.py
+++ api/models.py
@@ -9,7 +9,7 @@ class Author(models.Model):
 
     @property
     def display_name(self):
-        return f'{self.first_name} {self.last_name}' if self.first_name else '{self.last_name}'
+        return f'{self.first_name} {self.last_name}' if self.first_name else f'{self.last_name}'
 
     def __repr__(self):
         return f'Author(first_name={self.first_name}, last_name={self.last_name})'
