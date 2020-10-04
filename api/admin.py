007a4daaa5993635916c6c5b5533240bc8edbc84
voidnologo <(none)>
2020-10-03 21:15:16 -0400

    Add functionality to book admin

--- api/admin.py
+++ api/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from api import models
 
 
-admin.site.register(models.Book)
+@admin.register(models.Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'read')
+    list_filter = ('read', 'genre', 'age_group')
+    search_fields = ('title',)
+
+
+admin.site.register(models.Author)
