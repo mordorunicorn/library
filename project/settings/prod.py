24ad23abb168422d8b0fffaa77c06c90a679ef24
Jessica Gassman <jpwilson1293@gmail.com>
2022-10-30 00:11:24 -0500

    remove settings_secrets from prod settings

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -3,11 +3,8 @@ import dotenv
 import dj_database_url
 import os
 from .base import *  # noqa: F401, F403
-from . import settings_secrets
 
 ENV = 'prod'
-
-SECRET_KEY = settings_secrets.SECRET_KEY
 
 DEBUG = False
 
