50bd9c9e84fa1ce205e2cc1b18cd07a3ef6db46b
Jessica Gassman <jpwilson1293@gmail.com>
2022-11-01 10:07:48 -0500

    [DEBUG] turn debug off in production

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *  # noqa: F401, F403
 
 ENV = 'prod'
 
-DEBUG = True
+DEBUG = False
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,6 +21,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 ALLOWED_HOSTS = [
     'jessicas-library.herokuapp.com',
+    '127.0.0.1',
     '127.0.0.1:8000',
     'localhost',
 ]
