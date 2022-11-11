adbd156e74f00e172e28176cad453cd7007ba64d
Jessica Gassman <jpwilson1293@gmail.com>
2022-11-11 10:18:05 -0600

    remove unused Assets view

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,3 @@
-from .assets import Assets  # noqa: F401
 from .author_view import AuthorViewSet  # noqa: F401
 from .book_view import BookViewSet  # noqa: F401
 from .series_view import SeriesViewSet  # noqa: F401
