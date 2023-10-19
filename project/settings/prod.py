8d73b5e0865f84e8f86daca1a1dee2d3b162df9e
Jessica Gassman <jpwilson1293@gmail.com>
2023-10-18 23:15:55 -0500

    add django staticfiles

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -35,7 +35,7 @@ CSRF_COOKIE_SECURE = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(os.path.dirname(BASE_DIR), 'build/static'),
-    os.path.join(os.path.dirname(BASE_DIR), 'staticfiles'),
+    os.path.join(os.path.dirname(BASE_DIR), 'project', 'staticfiles'),
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
