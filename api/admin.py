39edaef1a6e298181905fce64a864a7c1f3aaaa4
Jessica Wilson <jpwilson1293@gmail.com>
2025-10-15 15:24:03 -0500

    add django actions to mark read and export csv

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
-import csv
+import csv, datetime
+
 from django import forms
 from django.contrib import admin
 from django.db.utils import IntegrityError
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.urls import path
 
@@ -14,6 +16,90 @@ class CsvImportForm(forms.Form):
     csv_file = forms.FileField()
 
 
+def mark_read(BookAdmin, request, queryset):
+    for obj in queryset:
+        obj.read = True
+        obj.save()
+
+
+mark_read.short_description = 'Mark as Read'
+
+
+def process_authors_for_csv(book, data_row):
+    authors = list(book.authors.all())
+    if len(authors) > 0:
+        data_row.append(authors[0].first_name)
+        data_row.append(authors[0].last_name)
+    else:
+        data_row.extend(['', ''])
+
+    if len(authors) > 1:
+        data_row.append(authors[1].first_name)
+        data_row.append(authors[1].last_name)
+    else:
+        data_row.extend(['', ''])
+
+    if len(authors) > 2:
+        data_row.append(authors[2].first_name)
+        data_row.append(authors[2].last_name)
+    else:
+        data_row.extend(['', ''])
+
+    return data_row
+
+
+def export_to_csv(BookAdmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;' 'filename=book_output.csv'
+    writer = csv.writer(response)
+    fields = [
+        'Title',
+        'Year',
+        'Author 1 First',
+        'Author 1 Last',
+        'Author 2 First',
+        'Author 2 Last',
+        'Author 3 First',
+        'Author 3 Last',
+        'Series',
+        'Series Number',
+        'Genre',
+        'Subgenre',
+        'Age Group',
+        'Audiobook',
+        'Read',
+        'Cover Url',
+        'Tags',
+    ]
+
+    # Write a first row with header information
+    writer.writerow(fields)
+    queryset = models.Book.objects.all()
+    # Write data rows
+    for book in queryset:
+        data_row = [book.title, book.year]
+        data_row = process_authors_for_csv(book, data_row)
+        data_row.append(book.series.name if book.series else '')
+        data_row.append(book.series_num or '')
+        data_row.extend(
+            [
+                book.subgenre.genre.name,
+                book.subgenre.name,
+                book.age_group,
+                'Y' if book.audiobook else 'N',
+                'Y' if book.read else 'N',
+                book.cover_url,
+                ','.join([tag.name for tag in book.tags.all()]),
+            ]
+        )
+
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export ALL as CSV'
+
+
 @admin.register(models.Author)
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ("first_name", "last_name")
@@ -23,6 +109,7 @@ class AuthorAdmin(admin.ModelAdmin):
 class BookAdmin(admin.ModelAdmin):
     change_list_template = "admin/entities/books_changelist.html"
 
+    actions = (mark_read, export_to_csv)
     list_display = ("title", "read", "author_display", "series", "series_num")
     list_filter = ("read", "subgenre", "age_group", "tags")
     search_fields = ("title", "authors__first_name", "authors__last_name", "series__name")
