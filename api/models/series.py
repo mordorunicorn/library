0d4dc43c8a215f949c0de168cebdc631981f3bd4
Jessica Wilson <jpwilson1293@gmail.com>
2025-10-15 11:37:50 -0500

    allow longer series names (#34)
    * allow longer series names
    
    * use Python 3.12
    
    * upgrade psycopg2

--- api/models/series.py
+++ api/models/series.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Series(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
